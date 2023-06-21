f6ea4000d9d1235e69bef553a239da42cb15fde5
Seheyda Dmitro <seheydad@gmail.com>
2023-06-21 15:08:43 +0300

    Initial Commit

---
+++ RikkiAndMorti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RikkiAndMorti
+//
+//  Created by Дмитро Сегейда on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
